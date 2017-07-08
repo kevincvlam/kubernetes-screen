44424e29c26ea3a10524eca64a4a885a9df673a2
kevin <kevin@kevins-MacBook-Pro.local>
2017-07-08 11:54:14 -0700

    more clean-up

--- clean-up-pipeline.sh
+++ clean-up-pipeline.sh
@@ -1,7 +1,12 @@
+# Clean-up completed pods
 kubectl delete -f producer.yaml
 kubectl delete -f consumer.yaml
 kubectl delete -f redis-service.yaml
 kubectl delete -f redis.yaml
+
+# Clean-up shared database
 bq rm -f -t database.values
 bq rm -f database
+
+# Turn down cluster
 gcloud container clusters delete kubernetes-screen
--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,11 +1,18 @@
+# Build the docker image and deploy it to the google container registry
 sudo docker build -t gcr.io/kubernetes-screen-173105/pipeline pipeline
 sudo gcloud docker -- push gcr.io/kubernetes-screen-173105/pipeline:latest
+
+# Start up GKE cluster with bigquery in its NODESCOPE so it can write to the 
+# database
 gcloud container clusters create kubernetes-screen --scopes bigquery
+
+# Create a BigQuery database and instaniate a table with teh schema in
+# schema.json
 bq mk database
 bq mk -t database.values schema.json
+
+# Start the pipeline 
 kubectl create -f redis.yaml
 kubectl create -f redis-service.yaml
-sleep 1
 kubectl create -f consumer.yaml
-sleep 1 
 kubectl create -f producer.yaml
